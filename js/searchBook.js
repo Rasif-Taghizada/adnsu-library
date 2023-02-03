16fc3091150128588c2e4143b8ea5b4cdcf4a844
Rasif <rasiftagizade11@gmail.com>
2023-02-03 12:37:06 +0400

    performans

--- js/searchBook.js
+++ js/searchBook.js
@@ -14,7 +14,9 @@ const loadCharacters = async () => {
 
 function displayCharacters(characters) {
     return characters.map((data, index) => {
-        bookList.innerHTML +=
+        if(index<100){
+            // console.log(data)
+            bookList.innerHTML +=
             `
                 <tr class="book ${data.janr.slice(0, 5).toLowerCase()}">
                     <td class="item title">
@@ -28,17 +30,41 @@ function displayCharacters(characters) {
                     </td>
                 </tr>
             `
+        }
+        else if(index>=100){
+            // console.log(data)
+            bookList.innerHTML +=
+            `
+                <tr class="book ${data.janr.slice(0, 5).toLowerCase()} hide">
+                    <td class="item title">
+                        <a href="#">${data.title}</a>
+                    </td>
+                    <td class="item author">
+                        <a href="#">${data.author}</a>
+                    </td>
+                    <td class="item janr">
+                        <a href="#">${data.janr}</a>
+                    </td>
+                </tr>
+            `
+        }
     })
 }
 
 const searchFilter = () => {
+    // select option janr
     const selectedJanr = document.querySelector("#filterByJanr").value.slice(0, 5);
-    console.log(selectedJanr);
+    // console.log(selectedJanr);
+    // Searching janr
     const input = document.querySelector("#search-input");
+    // Display data
     const booksData = document.getElementsByClassName("book")
+    // console.log(booksData);
+
     let textBox = input.value;
     for (let i = 0; i < booksData.length; i++) {
         let className = booksData[i].querySelector(".title");
+        // console.log(className)
         if (
             (booksData[i].classList.contains(selectedJanr) ||
                 selectedJanr == "") &&
